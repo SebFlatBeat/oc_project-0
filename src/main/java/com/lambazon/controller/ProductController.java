ab3103df86d3503d4dcb763b681b2d030f12b977
SebFlatBeat <sebastien.darre@gmail.com>
2018-08-17 17:19:57 +0200

    adjusting format code

--- src/main/java/com/lambazon/controller/ProductController.java
+++ src/main/java/com/lambazon/controller/ProductController.java
@@ -13,35 +13,36 @@ import com.lambazon.service.ProductService;
 
 @Controller
 public class ProductController {
-	
-	@Inject
-	private ProductService productService;
-	
-	@GetMapping("/products")
-	public String products	(Model model) {
-		model.addAttribute("prods", productService.products());
-		model.addAttribute("totalInventoryAmount", calculateTotalInventoryAmount());
-		return "products";
-	}
-	
-	@GetMapping("/products/{id}/details")
-	public String product	(@PathVariable Integer id, Model model) {
-		model.addAttribute("prod", productService.product(id));
-		return "product";
-	}
 
-	/**
-	 * Calculate total of the amount from stock
-	 * @return totalInventoryAmount
-	 */
-	private double calculateTotalInventoryAmount() {
-		double totalInventoryAmount = 0.0;
-		for (Product p : productService.products()) {
-			totalInventoryAmount+=p.getInventoryPrice();
-		}
-		
-		return totalInventoryAmount;
-		
-		
-	}
+    @Inject
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public String products(Model model) {
+        model.addAttribute("prods", productService.products());
+        model.addAttribute("totalInventoryAmount", calculateTotalInventoryAmount());
+        return "products";
+    }
+
+    @GetMapping("/products/{id}/details")
+    public String product(@PathVariable Integer id, Model model) {
+        model.addAttribute("prod", productService.product(id));
+        return "product";
+    }
+
+    /**
+     * Calculate total of the amount from stock
+     *
+     * @return totalInventoryAmount
+     */
+    private double calculateTotalInventoryAmount() {
+        double totalInventoryAmount = 0.0;
+        for (Product p : productService.products()) {
+            totalInventoryAmount += p.getInventoryPrice();
+        }
+
+        return totalInventoryAmount;
+
+
+    }
 }
